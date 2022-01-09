3974671a272864d59a6abdb415b60f80ee6b61c5
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-01-09 19:30:27 +0000

    Updates for AUv3 releases and revert some Windows issues

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -12,6 +12,7 @@
 
 #pragma once
 
+
 #include <atk_core/atk_core.h>
 #include <atk_eq/atk_eq.h>
 #include <atk_preamplifier/atk_preamplifier.h>
@@ -24,6 +25,7 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_core/juce_core.h>
+#include <juce_cryptography/juce_cryptography.h>
 #include <juce_data_structures/juce_data_structures.h>
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
@@ -32,22 +34,22 @@
 
 #include "BinaryData.h"
 
-#if defined(JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
-/** If you've hit this error then the version of the Projucer that was used to generate this project is
-    older than the version of the JUCE modules being included. To fix this error, re-save your project
-    using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
-    remove the JUCE_PROJUCER_VERSION define from the AppConfig.h file.
-*/
-#  error \
-      "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
+#if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
+ /** If you've hit this error then the version of the Projucer that was used to generate this project is
+     older than the version of the JUCE modules being included. To fix this error, re-save your project
+     using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
+     remove the JUCE_PROJUCER_VERSION define.
+ */
+ #error "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
 #endif
 
-#if !JUCE_DONT_DECLARE_PROJECTINFO
+
+#if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-const char* const projectName = "GuitarPreamp";
-const char* const companyName = "Matthieu Brucher";
-const char* const versionString = "2.0";
-const int versionNumber = 0x20000;
-} // namespace ProjectInfo
+    const char* const  projectName    = "GuitarPreamp";
+    const char* const  companyName    = "Matthieu Brucher";
+    const char* const  versionString  = "2.0";
+    const int          versionNumber  = 0x20000;
+}
 #endif
